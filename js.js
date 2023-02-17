bafef70f017c7a8d096a5038adfc9cb8942c93f0
Rafael <38193394+Rafael-Duarte-Silva@users.noreply.github.com>
2023-02-16 21:46:46 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,13 +1,23 @@
-const tema = document.getElementById("tema").innerText;
+const tema_texto = document.getElementById("tema").innerText;
+let palavra = "";
+const temas = {
+    animais: ["gato", "cachorro", "leão", "tigre", "elefante"],
+    frutas: ["maçã", "banana", "abacaxi", "laranja", "morango"],
+    países: ["brasil", "portugal", "espanha", "méxico", "argentina"],
+  };
 
-var palavras = ['a', 'b'];
+const tema = Object.keys(temas)[Math.floor(Math.random() * Object.keys(temas).length)];
+palavra = temas[tema][Math.floor(Math.random() * temas[tema].length)];
 
-let palavra = palavras.sort();
-console.log(palavra);
+document.getElementById("tema").innerText = tema_texto+" "+tema;
 
-document.getElementById("tema").innerText = tema+palavras;
-
-function sortear(){
-    console.log(Math.floor(Math.random() * 2));
-    return Math.floor(Math.random() * 2);
-}  
+for(let i = 0; i < palavra.length; i++){
+    let espacamentos = document.createElement('div');
+    espacamentos.setAttribute("id", "espacamentos");
+    espacamentos.style.width = "6vh";
+    espacamentos.style.height = "0.7vh";
+    espacamentos.style.marginLeft = "2vh";
+    espacamentos.style.marginTop = "10vh";
+    espacamentos.style.backgroundColor = "black";
+    document.getElementById("jogo").appendChild(espacamentos);
+}
